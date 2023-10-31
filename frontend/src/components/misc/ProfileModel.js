78d6432c3cff2d8125eaf17edf68b2514e677b80
me.Arun <93211897+ArunMehraa@users.noreply.github.com>
2023-10-31 18:10:06 +0530

    Update ProfileModel.js

--- frontend/src/components/misc/ProfileModel.js
+++ frontend/src/components/misc/ProfileModel.js
@@ -1,7 +1,6 @@
-import { useDisclosure } from '@chakra-ui/react';
+import { Avatar, useDisclosure } from '@chakra-ui/react';
 import { ViewIcon } from '@chakra-ui/icons';
 import { Button, IconButton, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
-
 import React from 'react';
 
 const ProfileModel = ({user,children}) => {
@@ -19,20 +18,21 @@ const ProfileModel = ({user,children}) => {
             />
         )}
 
-        <Modal size="lg" isOpen={isOpen} onClose={onClose} isCentered>
+        <Modal size="lg" isOpen={isOpen} onClose={onClose} isCentered >
          <ModalOverlay/>
-         <ModalContent h="410px">
-            <ModalHeader fontSize="40px" fontFamily="Work sans" display="flex" justifyContent="center" >{user.name}</ModalHeader>
+         <ModalContent h="410px" bg="linear-gradient(to right top, #05030c, #1b1531, #321d56, #50227a, #751f9c)" borderWidth={0.1} borderColor="gray">
+            <ModalHeader textColor="white" fontSize="40px" fontFamily="Work sans" display="flex" justifyContent="center" >{user.name}</ModalHeader>
             <ModalCloseButton/>
             <ModalBody display="flex" flexDir="column" alignItems="center" justifyContent="space-between" >
-                <Image borderRadius="full" boxSize="150px" src={user.pic} alt={user.name} />
-                <Text fontSize={{base:"28px",md:"30px"}} fontFamily="Work sans" >
+                {/* <Image borderRadius="full" boxSize="150px" src={user.pic} alt={user.name} /> */}
+                <Avatar boxSize="170px" borderRadius="full" name={user.name} src={user.pic} />
+                <Text textColor="white" fontSize={{base:"28px",md:"30px"}} fontFamily="Work sans" >
                     Email: {user.email}
                 </Text>
             </ModalBody>
 
             <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                <Button colorScheme="purple" mr={3} onClick={onClose}>
                     Close
                 </Button>
             </ModalFooter>
